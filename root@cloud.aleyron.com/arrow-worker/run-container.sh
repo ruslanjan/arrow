e130a17ffe648fb254719da873854b080d6e610c
ruslan jankurazov <ruslanjan888@gmail.com>
2019-10-16 17:47:02 +0600

    add media files volumes removed trash

--- root@cloud.aleyron.com/arrow-worker/run-container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -p 127.0.0.1:5000:5000 --env CELERY_BACKEND=redis://host.docker.internal:6379 --env CELERY_BROKER=redis://host.docker.internal:6379 --network host -it --cap-add=ALL --privileged --rm arrow-celery

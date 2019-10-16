208615ce4d694b2623a96818a3c1148be335e5ad
ruslan jankurazov <ruslanjan888@gmail.com>
2019-10-16 20:29:33 +0600

    WIP

--- arrow-worker/run-container.sh
+++ arrow-worker/run-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 127.0.0.1:5000:5000 --env CELERY_BACKEND=redis://host.docker.internal:6379 --env CELERY_BROKER=redis://host.docker.internal:6379 --network host -it --cap-add=ALL --privileged --rm arrow-celery
+docker run -p 127.0.0.1:5000:5000 --env CELERY_BACKEND=redis://:yourpassword@redis:6379 --env CELERY_BROKER=redis://:yourpassword@redis:6379 --network host -it --cap-add=ALL --privileged --rm arrow-celery

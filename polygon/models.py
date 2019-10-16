1559cd9338fcf86ea7c054ed8fe7830f3a9553ea
ruslan jankurazov <ruslanjan888@gmail.com>
2019-10-16 19:42:58 +0600

    fixed create test auto index

--- polygon/models.py
+++ polygon/models.py
@@ -58,7 +58,7 @@ class Test(models.Model):
     index = models.IntegerField()
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     is_example = models.BooleanField(default=False)
-    example_answer = models.TextField()
+    example_answer = models.TextField(blank=True, null=True)
     use_generator = models.BooleanField(default=False)
     generator = models.ForeignKey(Generator, null=True, blank=True,
                                   on_delete=models.SET_NULL)

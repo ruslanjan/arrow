69f35681fcd9b32a8aab457e20ea54bf75a1b3ba
ruslan jankurazov <ruslanjan888@gmail.com>
2019-10-16 17:19:42 +0600

    fix allowed hosts

--- arrow/settings.py
+++ arrow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f8s8^ig=rfwpr2^%fgdfdfce=d@obn7c=tp4&w_bdqswqa2(w)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [   '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 

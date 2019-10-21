9e53eec3c57a62e96ca564c965426c8963247f43
ruslan jankurazov <ruslanjan888@gmail.com>
2019-10-21 16:47:17 +0600

    added python 3.7 support

--- run-worker-container.sh
+++ run-worker-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # For dev
-docker run --env CELERY_BACKEND=redis://host.docker.internal:6379 --env CELERY_BROKER=redis://host.docker.internal:6379 --network host -it --cap-add=ALL --privileged --rm arrow-celery
+docker run --env CELERY_BACKEND=redis://host.docker.internal:6379 --env CELERY_BROKER=redis://host.docker.internal:6379 --network host -it --cap-add=ALL --privileged -v `pwd`/db.sqlite3:/app/db.sqlite3 --rm arrow-celery
